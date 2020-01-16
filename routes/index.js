4013a5f2588c2cdcbaaf709b9baac9212ca0cd2d
FilthyHound <k.gordon3@nuigalway.ie>
2020-01-16 11:32:01 +0000

    Added GET function calls
    Function calls added to retrieve username from users database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +10,31 @@ router.get('/', function(req, res, next) {
 router.get('/home', (req, res, next) => {
     res.render('home');
 })
+
+/*GET users, return usernames */
+router.get('/getUsers', (req, res, next) => {
+    User.find({},{user_name:1}, function (err, users) {
+        if (err)
+            res.send(err);
+
+         res.json(users);
+    });
+});
+
+/*GET user by ID, retrieves id & username */
+router.get('/getUser/:id', (req, res, next) => {
+    var id = req.params.id;
+    User.find({_id:id},{user_name:1}, (function (err, users) {
+        if(err)
+            res.send(err);
+
+        res.json(users);
+    }));
+});
+
+router.get('/profile', function(req, res, next) {
+    res.render('profile',{ welcome: 'profile page test' });
+});
 
 router.post('getTime', (req, res, next) => {
     const date = new Date();
@@ -25,14 +51,5 @@ router.get('/exampleBet', (req, res, next) => {
     res.render('exampleBet');
 })
 
-
-module.exports = router;
-router.get('/createBet', function(req, res, next){
-    res.render('create_bet', {title: 'CreateBet'});
-});
-
-router.get('/findBets', function(req, res, next){
-    res.render('find_bets', {title : 'FindBets'});
-});
 
 module.exports = router;
