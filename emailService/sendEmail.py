33b9cee596d36839c6e55b1e9635e2a8f1b377e9
iamcathal <cathalsocallaghan@gmail.com>
2020-01-16 12:50:25 +0000

    Unsecure email sending

--- emailService/sendEmail.py
+++ emailService/sendEmail.py
@@ -2,12 +2,8 @@
 import ezgmail
 import sys
 
-checkList = ["\"","`"]
-
-print("{} {} {}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
 if len(sys.argv) == 4:
-    if not any(x in str for x in checkList):
-        ezgmail.send(sys.argv[1], sys.argv[2], sys.argv[3])
-        print("[{}] {} - {}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    ezgmail.send(sys.argv[1], sys.argv[2], sys.argv[3])
+    print("[{}] {} - {}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
 else:
     print("Usage: python3 sendEmail.py emailAddress subjectText bodyText")
