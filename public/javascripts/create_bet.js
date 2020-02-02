77131017e66dbe510a34dfd9c91914590d54d596
Eoin McArdle <eoinmac995@gmail.com>
2020-02-02 22:32:09 +0000

    Code improvements complete for create_bet.js and maps.js

--- public/javascripts/create_bet.js
+++ public/javascripts/create_bet.js
@@ -1,10 +1,41 @@
+// Constants
+const DEFAULT_BET_VALS = {
+    name: 'New Bet',
+    location: 'Your location',
+    radius: 100
+}
+const RESTRICTIONS = {
+    min_bet_rad : 20
+}
+
+// Extends basic map class for more functionality
 class CreateBetMap extends Map{
     constructor(domID, options){
         super(domID, options);
+        this.betLayers = L.featureGroup();
+        this.userLocationMarker;
+        this.betCircle;
     }
     // Add marker to display user's location
     showUserLocation(userLocation){
-        L.marker(this.userLocation).addTo(this.map);
+        this.userLocationMarker = L.marker(userLocation).bindPopup('', {closeOnClick: false, closeButton : false, autoClose : false});
+        this.updateUserLocationMarker(DEFAULT_BET_VALS.name, DEFAULT_BET_VALS.location, DEFAULT_BET_VALS.radius);
+        this.betCircle = new BetArea(L.circle(userLocation, {
+            color : 'blue',
+            fillColor : 'blue',
+            fillOpacity : 0.2,
+            radius : 0
+        }));
+        this.betLayers.addLayer(this.betCircle.circle);
+        this.betLayers.addLayer(this.userLocationMarker);
+        this.betLayers.addTo(this.map);
+
+        this.userLocationMarker.openPopup();
+        this.betCircle.expand(DEFAULT_BET_VALS.radius);
+    }
+    updateUserLocationMarker(name, location, radius){
+        var str = '<b>' + name + '</b><br>Location: ' + location + '<br>Radius: ' + radius;
+        this.userLocationMarker._popup.setContent(str);
     }
     // Testing function to manually set user's location
     set userLocation(position){
@@ -15,11 +46,7 @@ class CreateBetMap extends Map{
     }
 }
 
-const MIN_BET_RAD = 20;
-
-// Object containing information about bet
-var betData = {title : 'New Bet', location_Name: 'location', latitude : 0, longitude : 0, radius: DEFAULT_RAD};
-
+// Load elements on page
 $(document).ready(async () => {
     // Map setup
     betMap = new CreateBetMap('mapID', options);
@@ -51,10 +78,13 @@ $(document).ready(async () => {
             console.log(result.message);
         }
     
-        betMap.showUserLocation(betMap.userLocation);
         const zoomDuration = 1;
         betMap.map.flyTo(betMap.userLocation, 15, {animate : true, duration: zoomDuration, easeLinearity : .43});
 
+        window.setTimeout(() => {
+            betMap.showUserLocation(betMap.userLocation);
+        }, zoomDuration * 1000);
+        
         setDOMDefaultValues(betNameDOM, locationDOM, radiusDOM, betMap.userLocation);
     
         // Get betting regions
@@ -64,54 +94,91 @@ $(document).ready(async () => {
         
         // Check if user is in one or more bet regions
         if(Array.isArray(regions) && regions.length){
-            betMap.displayBetRegions(regions, 'selectRegion', zoomDuration);
+            betMap.displayBetRegions(regions, 'selectRegion', zoomDuration).then(() => {
+                betMap.showUserLocation(betMap.userLocation);
+            });
         } else{
             console.log("No available betting regions")
         }
 
         // Create Bet/Bet region listeners
-        $('#createBet').click(addBetToDataBase);
-        $('#createBetRegion').click(addNewBetRegion);
-
-        
-        selectRegionDOM.change(() => {
-            var id = selectRegionDOM.val()
-            if(id != 'null'){
-                var layersToRemove = [];
-                betMap.betRegionLayer.eachLayer((layer) => {
-                    console.log("Leaflet id: " + layer._leaflet_id);
-                    console.log("Region id: " + id);
-                    if(!(layer._leaflet_id.toString() === id.toString())){
-                        layersToRemove.push(layer._leaflet_id);
-                    }else{
-                        console.log("Not equal");
-                    }
-                });
-                layersToRemove.forEach((id) =>{
-                    betMap.betRegionLayer.removeLayer(id);
-                });
-                console.log("Test");
-                betMap.betRegionLayer.addTo(betMap.map);
+        $('#createBet').click(() => {
+            // TODO write form validation
+            if(selectRegionDOM.val() != 'null'){
+                var newBet = {
+                    title: betNameDOM.val(),
+                    location_Name: locationDOM.val(),
+                    latitude: betMap.userLocation.lat,
+                    longitude: betMap.userLocation.lng,
+                    radius: parseInt(radiusDOM.val()),
+                    bet_region_id: selectRegionDOM.val().toString()
+                }
+                addBetToDataBase(newBet);
             }
+            else{
+                alert("Please choose a region to create bet");
+            }
+        });
+        $('#createBetRegion').click(() => {
+            // TODO write from validation
+            var newRegion = {
+                region_name : betNameDOM.val(), 
+                latitude : betMap.userLocation.lat, 
+                longitude : betMap.userLocation.lng,
+                radius : parseInt(radiusDOM.val()),
+                bet_ids : []
+            }
+            addNewBetRegion(newRegion);
+        });
+
+        // Allow user to choose which region to create bet in
+        selectRegionDOM.change(() => {
+            // Get selected region ID
+            var id = selectRegionDOM.val()
+
+            // Null corresponds to all regions
+            if(id != 'null'){
+                // Check if selected region is hidden
+                if(betMap.hiddenBetRegions.getLayer(id)){
+                    // Add to visible layer
+                    betMap.betRegionLayer.addLayer(betMap.hiddenBetRegions.getLayer(id));
+                    betMap.hiddenBetRegions.removeLayer(id);
+                }
+                // Remove other regions from visible layer
+                betMap.betRegionLayer.eachLayer((layer) => {
+                    if(!(layer._leaflet_id.toString() === id.toString())){
+                        betMap.hiddenBetRegions.addLayer(layer);
+                        betMap.betRegionLayer.removeLayer(layer);
+                    }        
+                });
+            }else{
+                // Display all available regions
+                betMap.hiddenBetRegions.eachLayer((layer) => {
+                    betMap.betRegionLayer.addLayer(layer);
+                });
+            }
+            // Add selected region(s) to map and open popups
+            betMap.betRegionLayer.addTo(betMap.map);
+            betMap.betRegionLayer.eachLayer((layer) => {
+                var subLayers = layer.getLayers();
+                subLayers.forEach((layer) =>{
+                    layer.openPopup();
+                });
+            });
         });
 
         // Update popUp when input fields are changed
-        locationDOM.keyup(function(){
-            betData.location = locationDOM.val();
-            marker._popup.setContent(formatPopUp(betData));
+        locationDOM.keyup(() => {
+            betMap.updateUserLocationMarker(betNameDOM.val(), locationDOM.val(), radiusDOM.val());
         });
-        
-        betNameDOM.keyup(function(){
-            betData.title = betNameDOM.val();
-            marker._popup.setContent(formatPopUp(betData));
+        betNameDOM.keyup(() => {
+            betMap.updateUserLocationMarker(betNameDOM.val(), locationDOM.val(), radiusDOM.val());
         });
-        
-        radiusDOM.keyup(function(){
-            betData.radius = radiusDOM.val();
-            marker._popup.setContent(formatPopUp(betData));
+        radiusDOM.keyup(() => {
+            betMap.updateUserLocationMarker(betNameDOM.val(), locationDOM.val(), radiusDOM.val());
             var val = parseInt(radiusDOM.val());
-            if(val >= MIN_BET_RAD){
-                area.setRadius(parseInt(radiusDOM.val()));
+            if(val >= RESTRICTIONS.min_bet_rad){
+                betMap.betCircle.expand(val);
             }
         });
 
@@ -119,9 +186,8 @@ $(document).ready(async () => {
 });
 
 function setDOMDefaultValues(name, location, rad, userLocation){
-    name.val('New Bet');
-    rad.val('50');
-    console.log(userLocation);
+    name.val(DEFAULT_BET_VALS.name);
+    rad.val(DEFAULT_BET_VALS.radius);
     // Reverse geocode lat and lng to predict location name of bet/betRegion
     $.ajax('https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=' + userLocation.lat + '&longitude=' + userLocation.lng + '&localityLanguage=en', 
     { 
@@ -131,6 +197,7 @@ function setDOMDefaultValues(name, location, rad, userLocation){
             location.val(predictedLocation);
         },
         error: (jqXHR, exception) => {
+            location.val(DEFAULT_BET_VALS.location);
             console.log("Location could not be found");
         }
     });
@@ -144,33 +211,24 @@ function getBettingRegions(location){
 }
 
 // Allows user to define new region where bets can be created and placed
-function addNewBetRegion(){
-    var betRegion = {
-        region_name : betNameDOM.val(), 
-        latitude : betData.latitude, 
-        longitude : betData.longitude,
-        radius : radiusDOM.val(),
-        bet_ids : []
-    }
-
-    $.post('/addBettingRegion', betRegion, function(data){
+function addNewBetRegion(newRegion){
+    $.post('/addBettingRegion', newRegion, function(data){
         console.log('Added bet region to database');
+        console.log(data);
     }, 'json');
 }
 
-function addBetToDataBase(){      
-    // Use put request to add bet to region
-    // Send bet region id and bet id
-
-    var dataToSend = {};
-    $.post('/addBetToDataBase', betData, function(res){
+// Allows user to add their bet to the database
+function addBetToDataBase(newBet){
+    var updateRegion = {};
+    $.post('/addBetToDataBase', newBet, function(res){
         console.log("Added data to database");
-        dataToSend.betID = res._id.toString();
-        dataToSend.regionID = $('#dropDown').val().toString();
+        updateRegion.betID = res._id.toString();
+        updateRegion.regionID = res.bet_region_id;
         $.ajax({
             url : '/addBetToRegion',
             type : 'PUT',
-            data : dataToSend,
+            data : updateRegion,
             success : function(res){
                 console.log(res);
             }
@@ -178,85 +236,8 @@ function addBetToDataBase(){
     }, 'json');
 }
 
-function addMapMarkers(pos){
-    //DEBUG setting location
-    pos = setLocation(pos);
-    // Check for level of accuracy
-    if(pos.accuracy > 100){
-        console.log("Location could not accurately be determined");
-
-    }
-    else{
-        // Include lat and long in data to add to database
-        var latLong = L.latLng(pos.latitude, pos.longitude);
-        betData.latitude = latLong.lat;
-        betData.longitude = latLong.lng;
-        
-        // Adding location marker to map
-        marker = L.marker(latLong);
-        marker.addTo(map);
-
-        // Reverse Geocoding
-        $.ajax('https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=' + pos.latitude + '&longitude=' + pos.longitude + '&localityLanguage=en', {success : function(data, status, xhr){
-            console.log(data);
-            // Extract user location    
-            var len = data.localityInfo.informative.length;
-            betData.location = data.localityInfo.informative[len - 1].name;
-            locationDOM.val(betData.location);
-            
-            // Create default marker and circle
-            marker.bindPopup(formatPopUp(betData), {closeButton : false, className: 'popUp'}).openPopup();
-            
-            const zoomDuration = 1;
-            
-            map.flyTo(latLong, 16, {animate : true, duration: zoomDuration, easeLinearity : .43});
-            
-            window.setTimeout(function(){
-                area = L.circle(latLong, {
-                color: 'blue',
-                fillColor: 'blue',
-                fillOpacity: 0.25,
-                radius: 0
-                }).addTo(map);
-                
-                betArea = new BetArea(area);
-                betArea.expand(DEFAULT_RAD);
-                
-            }, zoomDuration * 1000);
-            
-            
-            // Update popUp when input fields are changed
-            locationDOM.keyup(function(){
-                betData.location = locationDOM.val();
-                marker._popup.setContent(formatPopUp(betData));
-            });
-            
-            betNameDOM.keyup(function(){
-                betData.title = betNameDOM.val();
-                marker._popup.setContent(formatPopUp(betData));
-            });
-            
-            radiusDOM.keyup(function(){
-                betData.radius = radiusDOM.val();
-                marker._popup.setContent(formatPopUp(betData));
-                var val = parseInt(radiusDOM.val());
-                if(val >= MIN_BET_RAD){
-                    area.setRadius(parseInt(radiusDOM.val()));
-                }
-            });
-
-        }});
-    }
-}
-
-// Returns formatted string content of popup
-function formatPopUp(betData){
-    return '<b>' + betData.title + '</b><br>Location: ' + betData.location + '<br>Radius: ' + betData.radius;
-}
-
 // DEBUG function for setting user position
 function setLocation(position){
-
     //position.lat = 53.337840;
     //position.lng = -9.180165;
     
--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,4 +1,3 @@
-const DEFAULT_RAD = 100;
 const options = {
      TILESRC : 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
      METADATA : { attribution : 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors' },
@@ -11,8 +10,10 @@ class Map {
         this.id = domID;
         this.map;
         this.userLocation = null;
+        
         // Used to load visual elements
         this.betRegionLayer = L.layerGroup();
+        this.hiddenBetRegions = L.layerGroup();
         this.betLayer = L.layerGroup();
     }
     // Function to load map into DOM
@@ -30,48 +31,31 @@ class Map {
     }
     displayBetRegions(regions, dropDownID, zoomDuration){
         // Add visual elements to map
-        // Add marker, circle and popup to map to denote region
-        window.setTimeout(() => {
-            regions.forEach(betRegion => {
-                var latLng = L.latLng(betRegion.latitude, betRegion.longitude);
-                var marker = L.marker(latLng).bindPopup(betRegion.region_name, {closeButton : false, autoClose : false}).addTo(this.map);
-                marker.openPopup();
-                var circle = L.circle(latLng, {
-                    color : 'red',
-                    fillColor : 'red',
-                    fillOpacity : 0.2,
-                    radius : 0
+        return new Promise((resolve) => {
+            window.setTimeout(() => {
+                regions.forEach(betRegion => {
+                    var latLng = L.latLng(betRegion.latitude, betRegion.longitude);
+                    var marker = L.marker(latLng).bindPopup(betRegion.region_name, {closeOnClick: false, closeButton : false, autoClose : false}).addTo(this.map);
+                    marker.openPopup();
+                    var circle = L.circle(latLng, {
+                        color : 'red',
+                        fillColor : 'red',
+                        fillOpacity : 0.2,
+                        radius : 0
+                    });
+        
+                    var group = L.featureGroup([marker, circle]);
+                    group._leaflet_id = betRegion._id;
+                    var regionMarker = new BetArea(circle);
+                    regionMarker.expand(betRegion.radius);
+                    this.betRegionLayer.addLayer(group);
+                    $('#' + dropDownID).append('<option value=' + betRegion._id + '>' + betRegion.region_name + '</option>');
                 });
-    
-                var group = L.featureGroup([marker, circle]).bindPopup(betRegion.region_name, {closeButton : false});
-                group._leaflet_id = betRegion._id;
-                var regionMarker = new BetArea(circle);
-                regionMarker.expand(betRegion.radius);
-                this.betRegionLayer.addLayer(group);
-                $('#' + dropDownID).append('<option value=' + betRegion._id + '>' + betRegion.region_name + '</option>');
-            });
-            this.betRegionLayer.addTo(this.map);
-
-        }, zoomDuration * 1000);
+                this.betRegionLayer.addTo(this.map);
+                resolve();
+            }, zoomDuration * 1000);
+        });
     }
-}
-
-function loadMap(mapID){
-    // Create map
-    map = L.map(mapID);
-    L.tileLayer(TILESRC, METADATA).addTo(map);
-    map.locate(locateSettings).on('locationfound', createMap).on('locationerror', function(err){
-        console.log(err);
-        alert("Could not find location");
-    });
-    
-}
-
-function createMap(location){
-    console.log(location);
-    var evt = $.Event('locRetrieved');
-    evt.location = location;
-    $(window).trigger(evt);
 }
 
 // Adds functionality to circle marker
@@ -80,14 +64,17 @@ class BetArea {
         this.circle = circle;
     }
     expand(limRad){
-        var wait = setInterval((function(){
+        var wait = setInterval((() => {
             var currRad = this.circle.getRadius();
             if(currRad < limRad){
-                this.circle.setRadius(currRad + 10);
+                this.circle.setRadius(currRad + 1);
+            }
+            else if(currRad > limRad){
+                this.circle.setRadius(currRad - 1);
             }
             else{
                 clearInterval(wait);
             }
-        }).bind(this), 20);
+        }).bind(this), 1);
     }
 }
