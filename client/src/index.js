ce788eb7730470cd46542b3be793549818ba8c66
Eoin McArdle <eoinmac995@gmail.com>
2020-02-05 22:52:25 +0000

    Finished Cleanup

--- client/src/index.js
+++ client/src/index.js
@@ -1,75 +1,10 @@
 import React, {createRef, Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import './index.css';
-import DisplayMap from'./maps.js';
-
-class Bet extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      name: "Bet: " + this.props.num
-    }
-  }
-  render(){
-    return(
-      <div>{this.state.name}</div>
-    )
-  }
-}
-
-
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
-export default function SimpleCard() {
-  const classes = useStyles();
-
-  return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Example Bet: 1  
-        </Typography>
-        <Typography variant="h5" component="h2">
-          Location: NUIG
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Will it rain tomorrow?
-        </Typography>
-        <DisplayMap/>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Yes</Button>
-        <Button size="small">No</Button>
-      </CardActions>
-    </Card>
-  );
-}
-
+import Bets from'./bets.js';
 
 ReactDOM.render(
-    <SimpleCard />,
+    <Bets />,
     document.getElementById('root')
 )
 /*
