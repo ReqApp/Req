602e74b46cb2b4178eed97b4a8fcf376457bbbc2
EoinMcArdle99 <58373222+EoinMcArdle99@users.noreply.github.com>
2020-02-27 15:31:25 +0000

    Update bets.js

--- express/routes/bets.js
+++ express/routes/bets.js
@@ -48,13 +48,11 @@ bets.post('/addBetToDataBase', function(req, res, next){
 // API for getting bets in region
 bets.get('/getBetsInRegion', function(req, res, next){
     var id = req.query.id.toString();
-    Bet.find({bet_region_id : id}, function(betRegion, err){
+    Bet.find({bet_region_id : id}, function(bets, err){
         if(err){
-            console.log(err);
             res.json(err);
         }
         else{
-            console.log(bets);
             res.json(bets);
         }
     });
