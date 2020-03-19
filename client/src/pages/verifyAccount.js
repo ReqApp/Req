b5339841fb726710d98e5b9db652c09c2daea90a
Eoin McArdle <eoinmac995@gmail.com>
2020-03-19 21:37:30 +0000

    Converted verify account

--- client/src/pages/verifyAccount.js
+++ client/src/pages/verifyAccount.js
@@ -1,32 +1,63 @@
+// React
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
+import PropTypes from 'prop-types';
+// Material
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import GTranslateIcon from '@material-ui/icons/GTranslate';
-import GroupWorkIcon from '@material-ui/icons/GroupWork';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { withStyles } from '@material-ui/core/styles';
+// Components
+import Copyright from '../components/copyRight';
 
-function Copyright() {
+class VerifyAccount extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    const {classes} = this.props;
     return (
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://github.com/ReqApp">
-          Req
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Enter your verification code
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Verification Code"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Submit
+            </Button>
+          
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
     );
   }
+}
 
-const useStyles = makeStyles(theme => ({
+const classes = theme => ({
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
@@ -45,45 +76,10 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-}));
+});
 
-export default function VerifyAccount() {
-  const classes = useStyles();
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Enter your verification code
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Verification Code"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Submit
-          </Button>
-        
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+// Export class with material styles
+VerifyAccount.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+export default withStyles(classes)(VerifyAccount);
