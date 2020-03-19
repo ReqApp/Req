f8860e2d0fa11c3cfe21aabfceaa0d962ffe7c0f
Eoin McArdle <eoinmac995@gmail.com>
2020-03-19 21:26:08 +0000

    Sign-In Page Converted to Class

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,4 +1,7 @@
+// React
 import React from 'react';
+import PropTypes from 'prop-types';
+// Material
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -10,23 +13,107 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import GTranslateIcon from '@material-ui/icons/GTranslate';
 import GroupWorkIcon from '@material-ui/icons/GroupWork';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { withStyles } from '@material-ui/core/styles';
+// Components
+import Copyright from '../components/copyRight';
 
-function Copyright() {
-    return (
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://github.com/ReqApp">
-          Req
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
+class SignIn extends React.Component{
+  constructor(props){
+    super(props);
   }
 
-const useStyles = makeStyles(theme => ({
+  render(){
+    const {classes} = this.props;
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+
+            Sign in through a 3rd party application
+
+            <Grid container>
+              <Grid item lg>
+                <Link href="#" variant="body2">
+                <Avatar className={classes.avatar}>
+                  <GitHubIcon />
+                </Avatar>
+                </Link>
+              </Grid>
+              <Grid item lg>
+                <Link href="#" variant="body2">
+                <Avatar className={classes.avatar}>
+                  <GTranslateIcon />
+                </Avatar>
+                </Link>
+              </Grid>
+              <Grid item lg>
+                <Link href="#" variant="body2">
+                <Avatar className={classes.avatar}>
+                  <GroupWorkIcon />
+                </Avatar>
+                </Link>
+              </Grid>
+            </Grid>
+            
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    );
+  }
+}
+
+const useStyles = theme => ({
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
@@ -45,94 +132,10 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-}));
+});
 
-export default function SignIn() {
-  const classes = useStyles();
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-
-          Sign in through a 3rd party application
-
-          <Grid container>
-            <Grid item lg>
-              <Link href="#" variant="body2">
-              <Avatar className={classes.avatar}>
-                <GitHubIcon />
-              </Avatar>
-              </Link>
-            </Grid>
-            <Grid item lg>
-              <Link href="#" variant="body2">
-              <Avatar className={classes.avatar}>
-                <GTranslateIcon />
-              </Avatar>
-              </Link>
-            </Grid>
-            <Grid item lg>
-              <Link href="#" variant="body2">
-              <Avatar className={classes.avatar}>
-                <GroupWorkIcon />
-              </Avatar>
-              </Link>
-            </Grid>
-          </Grid>
-          
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+// Export class with material styles
+SignIn.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+export default withStyles(useStyles)(SignIn);
