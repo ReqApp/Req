302a4d54335965b8f5dd7ce240e175c751ea11ba
iamcathal <cathalsocallaghan@gmail.com>
2020-03-26 18:26:15 +0000

    Team details added on homepage

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -6,9 +6,14 @@ import {Paper} from '@material-ui/core';
 import EuroIcon from '@material-ui/icons/Euro';
 import ExploreIcon from '@material-ui/icons/Explore';
 import PeopleIcon from '@material-ui/icons/People';
-import logo from '../images/reqGif.gif'
 import ReqAnimation from '../components/reqAnimation';
 import Copyright from '../components/copyRight'
+import Avatar from '@material-ui/core/Avatar';
+
+import Cathal from '../images/cathalAvatar.png';
+import Karl from '../images/Karl.jpeg';
+import Rory from '../images/Rory.jpeg';
+import GitHubIcon from '../images/githubIcon.svg';
 
 
 export class Home extends Component {
@@ -25,7 +30,6 @@ export class Home extends Component {
                     </Row>
                     <Row>
                         <Col>
-              
                             <Paper elevation={3} style={styles.card}>
                                 <h4>Win Big</h4>
                                 <EuroIcon fontSize="large" style={styles.icon}/>
@@ -60,6 +64,35 @@ export class Home extends Component {
                                     <a href=""> Learn about how our betting system works </a>
                                 </Typography>
                             </Paper>
+                        </Col>
+                    </Row>
+
+                    <h1 style={{textAlign:'center'}}> The Team</h1>
+
+                    <Row style={{textAlign:'center', paddingBottom:'30px'}}>
+                        <Col>
+                           <Avatar alt="Cathal O'Callaghan - Back-end Infrastructure" src={Cathal} style={{width:'120px', height:'120px', margin:'12px auto 12px auto'}}></Avatar>
+                        <h4 style={{fontWeight:'bold'}}>Cathal O'Callaghan <a href="https://iamcathal.github.io"><img src={GitHubIcon} style={{height:'26px', width:'26px'}}></img></a></h4> 
+                        <h6> Back-end Infrastructure </h6>
+                        
+                        </Col>
+                        <Col>
+                           <Avatar alt="Karl Gordon - Front-end UI/UX" src={Karl} style={{width:'120px', height:'120px', margin:'12px auto 12px auto'}}></Avatar>
+                           <h4 style={{fontWeight:'bold'}}>Karl Gordon <a href="https://github.com/FilthyHound"><img src={GitHubIcon} style={{height:'26px', width:'26px'}}></img></a></h4>
+                           <h6> Front-end UI/UX </h6>
+
+                        </Col>
+                        <Col>
+                           <Avatar alt="Rory Sweeney -  - Front-end UI/UX" src={Rory} style={{width:'120px', height:'120px', margin:'12px auto 12px auto'}}></Avatar>
+                           <h4 style={{fontWeight:'bold'}}>Rory Sweeney <a href="https://github.com/RorySweeney99"><img src={GitHubIcon} style={{height:'26px', width:'26px'}}></img></a></h4>
+                           <h6> Front-end UI/UX </h6>
+
+                        </Col>
+                        <Col>
+                           <Avatar alt="Eoin Mc Ardle - Back-end Infrastructure/Front-end UI/UX" src={Cathal} style={{width:'120px', height:'120px', margin:'12px auto 12px auto'}}></Avatar>
+                           <h4 style={{fontWeight:'bold'}}>Eoin Mc Ardle <a href="https://github.com/EoinMcArdle99"><img src={GitHubIcon} style={{height:'26px', width:'26px'}}></img></a></h4>
+                           <h6> Back-end Infrastructure / Front-end UI </h6>
+
                         </Col>
                     </Row>
                     <Row>
