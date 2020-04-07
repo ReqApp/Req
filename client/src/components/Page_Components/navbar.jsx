0fed470799d0e5ac948f8047bf2f229b0c933919
IamCathal <cathalsocallaghan@gmail.com>
2020-04-07 21:59:30 +0100

    Navbar fix

--- client/src/components/Page_Components/navbar.jsx
+++ client/src/components/Page_Components/navbar.jsx
@@ -25,7 +25,7 @@ export default class Navbar extends React.Component{
             },
         }).then((res) => res.json())
         .then((res) => {    
-            this.setState({username:res.body});
+            this.setState({username:res.body.user_name});
             fetch('http://localhost:9000/users/getProfilePicture', {
             method: 'POST',
             crossDomain: true,
